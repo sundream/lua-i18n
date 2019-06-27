ed9e2e8e93fa5ff05d1d951086210765ed4f110c
linguangliang <linguangliang@jinkejoy.com>
2019-06-27 18:47:19 +0800

    to_lang => default_language

--- test.lua
+++ test.lua
@@ -46,8 +46,8 @@ local function test()
 	local packstr = i18n.format("测试字典参数,目标={target},npc={npc}",{target="目标",npc="npc90001"})
 	local str = i18n.translateto("en_US",packstr)
 	print(str)
-    -- 如果设置了to_lang字段,i18n.format将自动翻译
-    i18n.to_lang = "zh_CN"
+    -- 如果设置了default_language字段,i18n.format将自动翻译
+    i18n.default_language = "zh_CN"
 	local packstr = i18n.format("测试字典参数,目标={target},npc={npc}",{target="目标",npc="npc90001"})
     print(packstr)
 end
